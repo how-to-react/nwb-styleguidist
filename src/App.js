01bd8da0688dd513aee7bd6c95b25a31a6c88d0e
Stephen Fraser <stephen.fraser@digirati.com>
2019-10-20 22:37:05 +0100

    Check this commit for loading sass

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.scss'
 
 import React, {Component} from 'react'
 
--- styleguide.config.js
+++ styleguide.config.js
@@ -1,6 +1,15 @@
 var createNwbWebpackConfig = require('create-nwb-webpack-config');
 
+var config = createNwbWebpackConfig();
+
+// delete config.optimization;
+
+config.module.rules.push({
+  test: /\.s[ac]ss$/,
+  use: ['css-loader', 'sass-loader'],
+});
+
 module.exports = {
-  webpackConfig: createNwbWebpackConfig(),
+  webpackConfig: config,
   components: 'src/**/[A-Z]*.js'
 };
